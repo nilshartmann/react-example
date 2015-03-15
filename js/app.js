df27c9d4e5bccf5ad2a156f6e47f570a51b6ba06
Nils Hartmann <nils@nilshartmann.net>
2015-03-15 23:39:21 +0100

    ES6

--- js/app.js
+++ js/app.js
@@ -6,25 +6,31 @@
 // --- Copyright (c) 2015 Nils Hartmann (http://nilshartmann.net).
 // -------------------------------------------------------------------------------------
 
-var Header = React.createClass({
-	render: function () {
-		return (
-			<h1 className="grey-text text-darken-1">{this.props.text}</h1>
-		);
+class Header extends React.Component {
+	render() {
+		return <h1 className="grey-text text-darken-1">{this.props.text}</h1>;
 	}
-});
+}
 
-var HomePage = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<Header text="org.eclipse.osgi.equinox"/>
-			</div>
-		);
-	}
-});
-
+//var Header = React.createClass({
+//	render: function () {
+//		return (
+//			<h1 className="grey-text text-darken-1">{this.props.text}</h1>
+//		);
+//	}
+//});
+//
+//var HomePage = React.createClass({
+//	render: function () {
+//		return (
+//			<div>
+//				<Header text="org.eclipse.osgi.equinox"/>
+//			</div>
+//		);
+//	}
+//});
+//
 React.render(
-	<HomePage />,
+	<Header text='org.eclipse.osgi.equinox' />,
 	document.getElementById('homepage')
 );
